eaa66e266cb89654d89d44058a56e5cf0662b1a9
Samara2006 <83661958+Samara-maker@users.noreply.github.com>
2023-05-29 17:22:17 -0400

    att

--- js/usuario.js
+++ js/usuario.js
@@ -1,5 +1,23 @@
-const input = document.querySelectorAll('[data-getInput]');
-console.log(input);
+const inputs = document.querySelectorAll('[data-getInput]');
+inputs.forEach((elemento)=>{
+    elemento.addEventListener('blur',(evento)=>{
+        validaCampo(evento.target);
+    });
+});
 
 
 
+function validaCampo(campo)
+{
+    const msnErro = campo.parentNode.querySelector('[data-erro]');
+    if(campo.name ==='Nome')
+    {
+        if(campo.value.length<5)
+        {
+            msnErro.textContent = 'Digite o nome completo';
+
+        }else{
+            msnErro.textContent ="";
+        }
+    }
+}
