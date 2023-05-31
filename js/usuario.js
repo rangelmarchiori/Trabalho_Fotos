9886ea1a40b66cbccf6a75f2bf9d91327d59279f
Samara2006 <83661958+Samara-maker@users.noreply.github.com>
2023-05-31 16:10:44 -0400

    atu

--- js/usuario.js
+++ js/usuario.js
@@ -1,4 +1,30 @@
 const inputs = document.querySelectorAll('[data-getInput]');
+
+const form = document.getElementById('form');
+
+const campos = document.querySelectorAll('.required');
+
+const spans = document.querySelectorAll('.mensagem-erro');
+
+function setError(usuario)
+{
+    campos(usuario).style.border = '2px solid #e63636';
+    spans(usuario).style.display = 'none';
+}
+
+function nameValidate()
+{
+    if(campos[0].value.length <3)
+    {
+        setError(0);
+    }
+    else
+    {
+        removeError(0)
+    }
+}
+
+
 inputs.forEach((elemento)=>{
     elemento.addEventListener('blur',(evento)=>{
         validaCampo(evento.target);
@@ -7,17 +33,4 @@ inputs.forEach((elemento)=>{
 
 
 
-function validaCampo(campo)
-{
-    const msnErro = campo.parentNode.querySelector('[data-erro]');
-    if(campo.name ==='Nome')
-    {
-        if(campo.value.length<5)
-        {
-            msnErro.textContent = 'Digite o nome completo';
 
-        }else{
-            msnErro.textContent ="";
-        }
-    }
-}
